b3c6761a1afd64afa9bb655488517894721b95df
PratikGarai <pratikgarai0208@gmail.com>
2021-03-31 23:23:21 +0530

    feed component initialized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Feed from './components/Feed/Feed';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,11 @@
+import './Feed.css';
+
+const Feed = () => {
+    return (
+        <div className="feed">
+            Feed
+        </div>
+    )
+}
+
+export default Feed;
