071161678a7553c6472d7d7a072b8f22026b3c1b
PratikGarai <pratikgarai0208@gmail.com>
2021-04-06 04:44:40 +0530

    designed an article

--- src/components/Feed/Article/Article.js
+++ src/components/Feed/Article/Article.js
@@ -1,14 +1,18 @@
 import { Avatar } from '@material-ui/core';
+import PostOption from '../PostContainer/PostOption/PostOption';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 
 import './Article.css';
 
 const Article = ({name, description, message, photoUrl}) => {
     return (
         <div className="article">
-            Article
             <div className="article__header">
                 <Avatar />
-                <div className="aricle__info">
+                <div className="article__info">
                     <h2>{name}</h2>
                     <p>{description}</p>
                 </div>
@@ -16,6 +20,13 @@ const Article = ({name, description, message, photoUrl}) => {
 
             <div className="article__body">
                 <p>{message}</p>
+            </div>
+
+            <div className="article__buttons">
+                <PostOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+                <PostOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <PostOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <PostOption Icon={SendOutlinedIcon} title="Send" color="gray" />
             </div>
         </div>
     )
