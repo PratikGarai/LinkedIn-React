bfd2c3b695f123234c2292cdb87a35b4f0721014
PratikGarai <pratikgarai0208@gmail.com>
2021-04-11 03:47:10 +0530

    login page and state cinfigured

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
+import {useSelector} from "react-redux";
 
 import './App.css';
+import Login from './components/Login/Login';
 
-function App() {
+const App = () => 
+{
+  const user = useSelector(state => state.user.currentUser);
+
   return (
     <div className="App">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? <Login /> :
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      }
     </div>
   );
 }
